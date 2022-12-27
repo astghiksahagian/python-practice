6ad9c5d116cbf0c92ca91326df8faa782c0edf2a
Astghik Sahagian <astghiksahagian@gmail.com>
2022-12-27 00:13:27 -0800

    add exercise 9

---
+++ ex9.py
@@ -0,0 +1,27 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJul\nAug"
+#using /n allows for each word after it to be on a new line.
+    #Result: Jan
+    # Feb 
+    # Mar
+    # Apr 
+    # May 
+    # June 
+    # Jul 
+    # Aug
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There's something going on here. 
+With the three double quotes.
+We'll be able to type as much as we like. 
+Even 4 lines if we want, or 5, or 6.
+""")
+#using 3 double quotes allows for each phrase that is added onto a new line to be printed in that formatting as well.
+    #Result:
+    # There's something going on here. 
+    # With the three double quotes.
+    # We'll be able to type as much as we like. 
+    # Even 4 lines if we want, or 5, or 6.
