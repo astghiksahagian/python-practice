95319453931b4e9ed708c53f100533697ed88d2c
Astghik Sahagian <astghiksahagian@gmail.com>
2022-12-28 00:31:20 -0800

    add exercise 13: using argv from the sys module to be assigned to variables that will later be assigned to arguments that are passed down to it

---
+++ ex13.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+# script, first, second, third = argv
+
+# print("This script is called:", script)
+# print("Your first variable is:", first)
+# print("Your second variable is:", second)
+# print("Your third variable is:", third)
+
+script, love, that, look, on, you = argv
+
+print("This script is called:", script)
+print("Your love variable is:", love)
+print("Your that variable is:", that)
+print("Your look variable is:", look)
+print("Your on variable is:", on)
+print("Your you variable is:", you)
+
+fashion_favorite = input("What's your favorite type of clothing item or accessory? ")
+
+fashion_style = input("What's your favorite type of fashion style? ")
