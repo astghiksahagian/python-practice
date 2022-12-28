546b90407b3c06b4b8565ff6f985c0cb1d2fb35f
Astghik Sahagian <astghiksahagian@gmail.com>
2022-12-27 18:20:17 -0800

    add exercise 11: input() lesson

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print("How old are you?", end= " ")
+age = input()
+
+print("How tall are you?", end= " ")
+height = input()
+
+print(f"So, you're {age} years old and {height} tall.")
+
+
+programming_language = input("What programming language are you learning? ")
+satisfaction = input("Level of statisfaction: ")
+
+print(f"{programming_language} is a great programming language! I'm glad you are {satisfaction} with it so far.")
