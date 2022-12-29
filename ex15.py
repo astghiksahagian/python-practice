37f055fbdf2ba2da45b97d1b007a71d1b5a4e96d
Astghik Sahagian <astghiksahagian@gmail.com>
2022-12-28 20:33:40 -0800

    add exercise 15: use extra sample text file (ex15_sample.txt) to open and read file within ex15.py

---
+++ ex15.py
@@ -0,0 +1,19 @@
+from sys import argv
+
+script, filename = argv
+#takes 2 arguments
+
+txt = open(filename)
+#opens a file based on the one chosen in filename
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+#prints (reads) the text inside the filename file onto this file
+
+print("Type the filename again:")
+file_again = input("> ")
+#prompts user input to manually type name of the file being used
+
+txt_again = open(file_again)
+print(txt_again.read())
+#opens the file name stored inside of file_again and reads (prints) it using the .read() function.
