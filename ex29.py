1ef6869ce3f8f6b0959a7e4f7b29ee7dbe884666
Astghik Sahagian <astghiksahagian@gmail.com>
2023-01-21 16:38:41 -0800

    add exercise 29: pracitcing if statements using true and false evaluations.

---
+++ ex29.py
@@ -0,0 +1,42 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("too many cats! The world is doomed!")
+
+#too many cats! The world is doomed!
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+# n/a
+
+if people < dogs:
+    print("The world is drooled on!") 
+
+# n/a
+
+if people > dogs:
+    print("The world is dry")
+
+#The world is dry
+
+
+
+dogs += 5
+
+if people >= dogs:
+    print("People are gretater than or equal to dogs.")
+
+#People are gretater than or equal to dogs.
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+#People are less than or equal to dogs.
+
+if people == dogs:
+    print("People are dogs.")
+
+#People are dogs.
